69650325d1cbea5ab8cc8025ac171224075991e5
Wladimir Rocha <wld.rocha@gmail.com>
2024-01-20 12:57:52 -0400

    [REF] change items links

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -12,7 +12,7 @@ export const Sidebar = () => {
     },
     {
       path: '/categories',
-      title: 'Categories'
+      title: 'rest-todos'
     }
   ]
 
@@ -20,7 +20,6 @@ export const Sidebar = () => {
     <aside className='ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]'>
       <div>
         <div className='-mx-6 px-6 py-4'>
-          {/* TODO: Next/Link hacia dashboard */}
           <Link href='/dashboard'>
             <Image
               width={150}
@@ -33,7 +32,6 @@ export const Sidebar = () => {
         </div>
 
         <div className='mt-8 text-center'>
-          {/* Next/Image */}
           <Image
             width={150}
             height={150}
