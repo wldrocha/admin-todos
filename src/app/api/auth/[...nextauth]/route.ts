980740e92ecde9616fdc3a5605cde1da63f64aa7
Wladimir Rocha <wld.rocha@gmail.com>
2024-02-07 15:43:26 -0400

    [ADD] auth with prisma and auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,12 @@
+import prisma from '@/app/lib/prisma'
+import { PrismaAdapter } from '@auth/prisma-adapter'
 import NextAuth, { NextAuthOptions } from 'next-auth'
+import { Adapter } from 'next-auth/adapters'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import Image from 'next/image'
+
+import { redirect } from 'next/navigation'
 
 export default function Home() {
+  redirect('/dashboard/')
   return (
     <>
       <span className='text-5xl'>Hola mundo</span>
