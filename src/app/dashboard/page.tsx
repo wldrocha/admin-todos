3b08c3212fe00cb963533dd89185960a0eb5fd5c
Wladimir Rocha <wld.rocha@gmail.com>
2024-02-07 13:28:12 -0400

    [ADD] show user info

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,26 @@
 import { WidgetItem } from '@/components'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
+import Image from 'next/image'
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    redirect('api/auth/signin')
+  }
   return (
     <div>
-      <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
-        {/* <WidgetItem /> */}
+      <div className='grid gap-6 grid-cols-1 md:grid-cols-2'>
+        <WidgetItem title='Usuario conectado S-Side'>
+          <div className='flex flex-col'>
+            <Image src={session?.user?.image} width={100} height={100} alt={`image from ${session.user?.name}`} />
+            <span>{session.user?.name}</span>
+            <span>{session.user?.email}</span>
+            <span>{session.user?.image}</span>
+          </div>
+        </WidgetItem>
       </div>
     </div>
   )
