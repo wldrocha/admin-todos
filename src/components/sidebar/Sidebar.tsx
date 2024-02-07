df984b2f88d30a549c407f8f29038a7a41628386
Wladimir Rocha <wld.rocha@gmail.com>
2024-02-07 13:28:43 -0400

    [REF] change static user info

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,8 +3,10 @@ import Link from 'next/link'
 import { CiLogout } from 'react-icons/ci'
 import { SidebarItem } from '.'
 import Image from 'next/image'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
   const menuItems = [
     {
       path: '/dashboard',
@@ -28,6 +30,13 @@ export const Sidebar = () => {
     }
   ]
 
+  const session = await getServerSession(authOptions)
+
+  const userName = session?.user?.name ?? 'No Name'
+
+  const avatarUrl =
+    session?.user?.image ?? 'https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp'
+
   return (
     <aside className='ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]'>
       <div>
@@ -47,11 +56,11 @@ export const Sidebar = () => {
           <Image
             width={150}
             height={150}
-            src='https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp'
-            alt=''
+            src={avatarUrl}
+            alt={`image of ${userName}`}
             className='w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28'
           />
-          <h5 className='hidden mt-4 text-xl font-semibold text-gray-600 lg:block'>Cynthia J. Watts</h5>
+          <h5 className='hidden mt-4 text-xl font-semibold text-gray-600 lg:block'>{userName}</h5>
           <span className='hidden text-gray-400 lg:block'>Admin</span>
         </div>
 
