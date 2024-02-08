28c661857bca220a3a119e62858ac43457950f67
Wladimir Rocha <wld.rocha@gmail.com>
2024-02-08 00:17:10 -0400

    [ADD] user todos

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,12 +1,18 @@
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
+
 //Lineas para revalidar la nformacion simepre, no funciona en fuera de page, layoute o route handler
 
 import prisma from '@/app/lib/prisma'
+import { getUserServerSession } from '@/auth'
 import { NewTodo, TodosGrid } from '@/todos'
+import { redirect } from 'next/navigation'
 
 export default async function RestTodoPage() {
-  const todos = await prisma.todo.findMany({ orderBy: { description: 'asc' } })
+  const user = await getUserServerSession()
+  console.log("🚀 ~ RestTodoPage ~ user:", user)
+  if (!user) redirect('/api/auth/signin')
+  const todos = await prisma.todo.findMany({ where: { userId: user.id }, orderBy: { description: 'asc' } })
 
   return (
     <div>
